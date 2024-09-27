6a36729aa7039b6acf7396f4842f92533a1b4697
Tates896 <tates4015@gmail.com>
2024-09-27 18:53:36 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "2349030430633"
+global.ownername = "Joshua tates"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '2349030430633'  //creator number
+global.ownername = 'Joshua tates' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
